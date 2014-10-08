bd96e07b1818700a78db9e31ee3eb53b24d5ac69
Josh Horwitz <jhorwit2@gmu.edu>
2014-10-08 19:12:35 -0400

    retrieval of top 100 done

--- app.js
+++ app.js
@@ -4,20 +4,29 @@ var Firebase = require('firebase'),
     async = require('async');
 
 // Get a reference to our posts
-var postsRef = new Firebase("https://hacker-news.firebaseio.com/v0/topstories");
+var topRef = new Firebase("https://hacker-news.firebaseio.com/v0/topstories/"),
+    itemRef = new Firebase("https://hacker-news.firebaseio.com/v0/item/");
 
-// Attach an asynchronous callback to read the data at our posts reference
-postsRef.on('value', function (snapshot) {
+// // Attach an asynchronous callback to read the data at our posts reference
+topRef.on('value', function (snapshot) {
     async.each(snapshot.val(), function (id, callback) {
 
+        itemRef.child(id).once('value', function (snapshot) {
+            console.log(snapshot.val().title);
+            callback();
+        });
     }, finished);
 
 }, error);
+
 
 var error = function (error) {
     console.log('The read failed: ' + errorObject.code);
 };
 
 var finished = function (error) {
+    if(itemRef) {
+        itemRef.off();
+    }
     console.log(error || 'success');
 };
