a476fdc348de19edc250f3ae48e586bfa34af30b
Josh Horwitz <jhorwit2@gmu.edu>
2014-10-08 22:18:53 -0400

    refactored into semi MVC architecture

--- app.js
+++ app.js
@@ -1,46 +1,4 @@
 'use strict';
 
-var Firebase = require('firebase'),
-    async = require('async'),
-    fs = require('fs'),
-    winston = require('winston');
-
-// Get a reference to our posts
-var topRef = new Firebase("https://hacker-news.firebaseio.com/v0/topstories/"),
-    threadRef = new Firebase("https://hacker-news.firebaseio.com/v0/item/"),
-    file = fs.createWriteStream('output.txt'),
-    logger = new (winston.Logger)({
-        transports: [
-          new (winston.transports.Console)({'timestamp':true}),
-          new (winston.transports.File) ({filename: 'audit.log'})
-        ]
-    });
-
-// // Attach an asynchronous callback to read the data at our posts reference
-topRef.on('value', function (snapshot) {
-    var top100 = snapshot.val();
-    async.each(top100, processThread, finished);
-}, error);
-
-var processThread = function (id, callback) {
-    threadRef.child(id).once('value', function (snapshot) {
-        var title = snapshot.val().title;
-        file.write(title + '\n');
-        return callback();
-    });
-};
-
-var error = function (error) {
-    logger.error('The read failed: ' + error.code)
-};
-
-var finished = function (error) {
-    // Clear the callbacks for threadRef
-    threadRef.off();
-
-    if (error) {
-        logger.error('Error:', error);
-    } else {
-        logger.info('Successfully updated output.txt');
-    }
-};
+// Start the application
+require('./controller/threads').start();
---
+++ model/thread.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var threadRef = require('../config/firebase').thread;
+
+// // Attach an asynchronous callback to read the data at our posts reference
+module.exports.create = function (id, callback) {
+    threadRef.child(id).once('value', function (snapshot) {
+        // Clear the callbacks for threadRef
+        threadRef.off();
+
+
+        var title = snapshot.val().title;
+        console.log(title);
+        //file.write(title + '\n');
+        return callback();
+    });
+};
